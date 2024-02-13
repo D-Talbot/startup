e1d2c95f0702819ce6a642842cae28849e81234c
Danica <dt604@byu.edu>
2024-02-13 13:30:19 -0700

    random words + psuedo code

---
+++ play.js
@@ -0,0 +1,7 @@
+// randomize 8 letter word chosen 
+// logic for displaying letter once correct one is chosen
+// greying out button color if incorrect letter choice
+// decrease the guesses left value 
+// save score to scoreboard (and other data)
+
+// reset game -> choose new word clear board 
