0c25d4b9805ba716b34c180a57b46a4967d0ac14
Danica <dt604@byu.edu>
2024-02-14 15:01:27 -0700

    scoreboard JS + login

---
+++ scores.js
@@ -0,0 +1,36 @@
+function loadScores() {
+    let scores = [];
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+      scores = JSON.parse(scoresText);
+    }
+  
+    const tableBodyEl = document.querySelector('#scores');
+  
+    if (scores.length) {
+      for (const [i, score] of scores.entries()) {
+        const rankTdel = document.createElement('td');
+        const rescuerTdel = document.createElement('td');
+        const duckssavedTdEl = document.createElement('td');
+        const dateTdEl = document.createElement('td');
+  
+        rankTdel.textContent = i + 1;
+        rescuerTdel.textContent = score.name;
+        duckssavedTdEl.textContent = score.score;
+        dateTdEl.textContent = score.date;
+  
+        const rowEl = document.createElement('tr');
+        rowEl.appendChild(rankTdel);
+        rowEl.appendChild(rescuerTdel);
+        rowEl.appendChild(duckssavedTdEl);
+        rowEl.appendChild(dateTdEl);
+  
+        tableBodyEl.appendChild(rowEl);
+      }
+    } else {
+      tableBodyEl.innerHTML = '<tr><td colSpan=4>What are you doing?! There is no time to waste. Get back to saving ducks <i>ASAP</i> !!</td></tr>';
+    }
+  }
+  
+  loadScores();
+  
