1e49e86ef77c807adb84339370150b8a97c2fa4b
Danica <dt604@byu.edu>
2024-04-04 16:17:44 -0600

    Create view components(login, play, abou, scores)

---
+++ react-template/src/about/about.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function About() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>about is displayed here</div>
+    </main>
+  );
+}
