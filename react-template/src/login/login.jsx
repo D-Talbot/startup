d58c4f319fce2d6aec6774c807cb2b62381fe919
Danica <dt604@byu.edu>
2024-04-05 13:09:49 -0600

    code clean up + play/login comp

--- react-template/src/login/login.jsx
+++ react-template/src/login/login.jsx
@@ -1,41 +1,34 @@
 import React from 'react';
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authState';
+import duck2 from '../../public/duck2.png';
 
-export function Login() {
+export function Login({ userName, authState, onAuthChange }) {
   return (
     <main className="container-main">
-        <img src="duck2.png" alt="duck" width="200"/>
+        <img src={duck2} alt="duck" width="200"/>
         <div>
-            <h2>Are you ready to save some ducks?</h2>
-                <div id="loginControls" style={{display: "none"}}>
+            {authState !== AuthState.Unknown && <h2>Are you ready to save some ducks?</h2>}
+                <div style={{display: "none"}}>
                     <p>Login now to start rescuing!</p>
-                    <div className="mb-3">
-                        <form method="get">
-                            <label for="username" className="form-label">Username: </label>
-                            <input type="text" className="form-control" style={{marginBottom: "10px"}} id="username" placeholder="Your username"/>
-                            <label for="password" className="form-label">Password: </label>
-                            <input type="text" className="form-control" id="password" placeholder="Your password"/>
-                            <div>
-                                <button className="button-primary" type="button" onclick="loginUser()">Login</button>
-                                <button className="button-primary" type="button" onclick="createUser()">Create Account</button>
-                            </div>
-                        </form>
-                    </div>
-                <div id="playControls" style={{display: "none"}}>
+                    {authState === AuthState.Authenticated && (
+                    <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+                    )}
+                    {authState === AuthState.Unauthenticated && (
+                    <Unauthenticated
+                        userName={userName}
+                        onLogin={(loginUserName) => {
+                        onAuthChange(loginUserName, AuthState.Authenticated);
+                        }}
+                    />
+                    )}
+                <div style={{display: "none"}}>
                     <div id="playerName"></div>
                     <p>Start rescuing ASAP!</p>
                     <button type="button" className="button-primary" onclick="logout()">Logout</button>
                   </div>
                 </div>
-                <div className="modal fade" id="msgModal" tabindex="-1">
-                    <div className="modal-dialog modal-dialog-centered">
-                        <div className="modal-content text-dark">
-                            <div className="modal-body">ERROR</div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        </div>
-                        </div>
-                    </div>
-                </div>   
         </div>
     </main>
   );
